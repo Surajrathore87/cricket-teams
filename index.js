36c60b263725b58a7cf3bdc2c9d82dd5a7859d8d
suraj.rathore8100 <suraj.rathore8100@gmail.com>
2024-12-14 19:40:10 +0530

    add

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+
+// const app = express();
+// const PORT = 3000;
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.set('view engine', 'ejs');
+
+// // MongoDB ConnectionAdmin123
+// const mongoURI = 'mongodb+srv://surajrathore8100:Admin123@cluster0.gm6yf.mongodb.net/'; // Replace with your MongoDB URI
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB connected'))
+//     .catch(err => console.log(err));
+
+// // Routes
+// const indexRoutes = require('./routes/index');
+// app.use('/', indexRoutes);
+
+// const playerRoutes = require('./routes/player');
+// app.use('/player', playerRoutes);
+
+// // Start Server
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-
-// MongoDB ConnectionAdmin123
-const mongoURI = 'mongodb+srv://surajrathore8100:Admin123@cluster0.gm6yf.mongodb.net/'; // Replace with your MongoDB URI
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-// Routes
-const indexRoutes = require('./routes/index');
-app.use('/', indexRoutes);
-
-const playerRoutes = require('./routes/player');
-app.use('/player', playerRoutes);
-
-// Start Server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
 });
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
